edfd7c776350ffbaa6b003acc51388addd643558
Andrei Konovalov <flotazani@gmail.com>
2025-03-05 00:19:20 -0800

    [mainline] NetworkManager base setup

---
+++ FetchHomeAssignment/Network/NetworkManager.swift
@@ -0,0 +1,18 @@
+//
+//  NetworkManager.swift
+//  FetchHomeAssignment
+//
+//  Created by Andrei Konovalov on 05.03.2025.
+//
+
+import Foundation
+
+final class NetworkManager {
+    public static var shared = NetworkManager()
+
+    private init() {}
+
+    public func loadRecipes() {}
+
+    public func loadImage() {}
+}
