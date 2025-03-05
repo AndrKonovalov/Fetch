3cf8ae5cee5f85c74fc418e10752c9ba49afb9ee
Andrei Konovalov <flotazani@gmail.com>
2025-03-05 00:01:31 -0800

    Initial Commit

---
+++ FetchHomeAssignment/FetchHomeAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchHomeAssignmentApp.swift
+//  FetchHomeAssignment
+//
+//  Created by Andrei Konovalov on 05.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct FetchHomeAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
