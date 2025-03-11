2b6f309420ad208ca6e1a0e617ed531ef3eb662e
Andrei Konovalov <flotazani@gmail.com>
2025-03-10 20:55:41 -0700

    [mainline] create RecipeViewModel and errors

---
+++ FetchHomeAssignment/Network/CustomErrors.swift
@@ -0,0 +1,14 @@
+//
+//  CustomErrors.swift
+//  FetchHomeAssignment
+//
+//  Created by Andrei Konovalov on 09.03.2025.
+//
+
+import Foundation
+
+
+enum CustomError: Error {
+    case NoImageUrl
+    case InvalidUUID
+}
