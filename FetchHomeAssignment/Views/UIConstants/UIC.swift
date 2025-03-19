ab7a910178972eb798a2cf3d61267f641e3ed9e7
Andrei Konovalov <flotazani@gmail.com>
2025-03-18 23:39:01 -0700

    [mainline] rename and update UIConstants

---
+++ FetchHomeAssignment/Views/UIConstants/UIC.swift
@@ -0,0 +1,16 @@
+//
+//  UIConstants.swift
+//  FetchHomeAssignment
+//
+//  Created by Andrei Konovalov on 06.03.2025.
+//
+
+import Foundation
+
+enum UIC {
+    static let padding: CGFloat = 8
+    static let cornerRadius: CGFloat = 15
+    static let shadow: CGFloat = 3
+    static let listImageWidth: CGFloat = 75
+    static let shadowOpacity: Float = 0.3
+}
